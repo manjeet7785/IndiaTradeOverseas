04d142c817389da428671704abde85feb80c0138
Manjeet Kumar Maurya <manjeetmaurya7785@gmail.com>
2026-06-24 21:24:29 +0530

    Update some api

--- Server/src/utils/otp.js
+++ Server/src/utils/otp.js
@@ -2,7 +2,6 @@ function generateOtp() {
     const otp = Math.floor(100000 + Math.random() * 900000);
     return otp.toString();
 }
-
 function getOtpHtml(otp) {
     return `<!DOCTYPE html>
 <html lang="en">
