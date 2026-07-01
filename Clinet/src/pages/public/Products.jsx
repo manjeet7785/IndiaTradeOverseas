678b1dcc07767bf4bfacd77f53ebeb9cacb229e2
Manjeet Kumar Maurya <manjeetmaurya7785@gmail.com>
2026-07-01 16:14:17 +0530

    Update Products.jsx
    Replaced multiple product entries with a new product entry for 'Purity Natural Black Coal'.

--- Clinet/src/pages/public/Products.jsx
+++ Clinet/src/pages/public/Products.jsx
@@ -80,40 +80,20 @@ export default function Products() {
     {
       id: 1,
      origin: 'India',
-     price: '20-50/kg',
-     name: 'RM Natural Unpolished Decorative Pebbles for Home & Garden Decor',
-     image: 'https://m.media-amazon.com/images/I/61FSFnx6r-L._SL1024_.jpg',
-     category: 'construction',
-     description: 'RM Natural Unpolished Decorative Pebbles for Home & Garden Decor (5kg, Rainbow, 20-50mm) | Raw Natural Stones for Landscaping, Plant Pots, Fillers, Aquarium, Pathways, Indoor Outdoor Use'
-  },
-  {
-    id: 2,
-    origin: 'India',
-    price: '20-50/kg',
-    name: 'Solid Natural Stone ',
-    image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSwiWjzogncwMWMQlEe7c4SMhst0Rle2wP7KS9PYFWU0FmAqF4zXlPU0XT9fKdA1v4MDDMoqhkqnthlc5Qwnada7xkk1BMW',
-    category: 'construction',
-    description: 'Solid Natural Stone- 60 mm Size, Heat-Resistant and Durable for Building Applications'
-   },
-    {
-      id: 3,
-      origin: 'India',
-      price: '20-50/kg',
-      name: 'Reflectix Expansion Joint',
-      image: 'https://m.media-amazon.com/images/I/41AWyJc1pWL._AC_UF1000,1000_QL80_.jpg',
-      category: 'construction',
-      description: 'Reflectix Expansion Joint.'
-    },
-    {
-      id: 4,
-      origin: "India",
-      price: '20-50/kg',
-      name: 'Gfrp Fiberglass Bar, Epoxy Composite Fiberglass Rebar',
-      image: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTz7SO8-Tud-Feg53A0TptPFRY6zRCkI7Z5Abg_KC4fCccB7MSJWB9rDk7yZr-to8vgPoVt42xMlRJP6YY4JTVhuM0WUBDM",
-      category: 'fiberglass_rebar',
-      description: 'Gfrp Fiberglass Bar, Epoxy Composite Fiberglass Rebar'
-    }
+     name: ' Purity Natural Black Coal For Industrial',
+     image: 'https://tiimg.tistatic.com/fp/1/008/230/99-purity-natural-black-coal-for-industrial-use-842.jpg',
+     category: 'Coal',
+     description: 'Key Features
+
+Purity : 99%
+Ash Content : 40-60%
+Type : Natural
+Color : Black
+Application : Steaming
+Volatile Matter : 0-15%'
+  }
   ];
+
 
   const categories = [
     { value: 'all', label: 'All Products' },
